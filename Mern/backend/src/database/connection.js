da939e92d23b56c6b333b216b712b92d08dbb87f
DanielC001 <danielcaro1000mail.com>
2021-10-16 11:58:34 -0500

    e1

--- Mern/backend/src/database/connection.js
+++ Mern/backend/src/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URL_ATLAS = 'mongodb+srv://admin:admin123@cluster0.f2hkf.mongodb.net/prueba?retryWrites=true&w=majority';
+const URL_ATLAS = 'mongodb+srv://admin:@cluster0.f2hkf.mongodb.net/prueba?retryWrites=true&w=majority';
 
 mongoose.connect(URL_ATLAS, {useNewUrlParser:true, useUnifiedTopology:true});
 
