a53acdfaca189c8e5834163950176a2fb4e4d7f7
AlexanderBarona <90266472+AlexanderBarona@users.noreply.github.com>
2021-10-19 19:39:15 -0500

    Delete nodmong directory
    eliminando carpeta de prueba

--- nodmong/src/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose= require('mongoose');
-mongoose.connect('mongodb+srv://adminpd@cluster0.wz9o2.mongodb.net/ProductosGwen') 
-
-
-.then(db=> console.log('Conectado a la DB Gwen'))
-.catch(err=> console.error(err));
